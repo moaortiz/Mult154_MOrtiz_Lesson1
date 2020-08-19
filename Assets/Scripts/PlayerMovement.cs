a1e186268643657ec62961496ba30498826e80f7
Monika Ortiz <monika.ortiz.aof@gmail.com>
2020-08-19 10:31:08 -0700

    Half way through first lesson

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    
+        
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+     void Update()
+    {
+       float horMove = Input.GetAxis("Horizontal");
+
+        rbPlayer.AddForce(new Vector3(horMove, 0, 0),ForceMode.Impulse);
+    }
+}
